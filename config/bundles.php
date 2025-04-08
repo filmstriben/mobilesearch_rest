91bf4ca9a5a980a568c580e38faf43088c63c785
Dinu Rodnitchi <rodnitchi@gmail.com>
2025-04-08 18:09:17 +0300

    FIL-2065: Enable basic auth for docs. Add bundles and security file.

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
